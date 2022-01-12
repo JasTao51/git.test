e78cf7a54a58603a1bfd631d68007212c7830098
JasTao <1429582832@qq.com>
2022-01-12 18:38:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author JasTao
+ * @date 2022/1/12 - 18:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+}
