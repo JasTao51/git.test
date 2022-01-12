1134878cc923f61df67a20c40070ec10f07a59f0
JasTao <1429582832@qq.com>
2022-01-12 22:03:39 +0800

    push commit

---
+++ SRC/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ * @author JasTao
+ * @date 2022/1/12 - 22:00
+ */
+public class GitTest {
+        public static void main(String[] args) {
+            System.out.println("hello.git!");
+            System.out.println("hello.git2!");
+            System.out.println("hello.git3!");
+            System.out.println("hello.git4!");
+            System.out.println("master test!");
+            System.out.println("hot-fix test!");
+            System.out.println("push test!");
+            System.out.println("pull test!");
+        }
+    }
